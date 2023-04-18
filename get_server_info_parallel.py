64f229beb1d686e42adece6539bd008c402a7c09
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2023-04-17 22:50:29 -0300

    feat: script that uses concurrent implementation

---
+++ get_server_info_parallel.py
@@ -0,0 +1,76 @@
+import asyncio
+import json
+
+list = [
+    "Akita",
+    "Borzoi",
+    "Chow",
+    "otterhound",
+    "ovcharka", 
+    "papillon",
+    "pekinese",
+    "pembroke",
+    "rottweiler",
+    "saluki",
+    "samoyed",
+    "schipperke",
+    "schnauzer",
+    "basenji",
+    "beagle",
+    "bluetick",
+    "borzoi",
+    "bouvier",
+    "boxer",
+    "brabancon",
+    "briard",
+    "buhund",
+]
+
+BASE_URL="https://dog.ceo/api/breed/"
+BASE_RESOURCE="/images/random"
+
+CURL="/usr/bin/curl"
+CURL_SILENT_OPTION="-s"
+
+InfoResponse = dict[str, str]
+
+async def get_info(url: str) -> InfoResponse:
+    """Send HTTP requests"""
+    print("[DEBUG] Fetching {}".format(url))
+    response = {}
+
+    process = await asyncio.create_subprocess_shell(
+        " ".join([CURL, CURL_SILENT_OPTION, url]),
+        stdout=asyncio.subprocess.PIPE
+    )
+    stdout, stderr = await process.communicate()
+    response["stdin"] = url
+    response["stdout"] = json.loads(stdout)
+    response["stderr"] = stderr
+    return response
+
+def report(output_list: list):
+    """"Report output presentation"""
+
+    for item in output_list:
+        item_name :str = item["stdin"].split("/")[5]
+        print(item_name)
+
+        if item["stderr"] is not None:
+            print("\t [ERROR]", item["stderr"])
+            next
+        
+        print("\t", item["stdout"]["message"])
+
+
+async def execute_requests():
+    endpoints = ["{}{}{}".format(BASE_URL, item.lower(), BASE_RESOURCE) for item in list]
+
+    response_list = await asyncio.gather(
+        *[get_info(endpoint) for endpoint in endpoints]
+    )
+
+    report(response_list)
+
+if __name__ == "__main__":
+    asyncio.run(execute_requests())
